b86c85332fc18fdc7d4c5bf14977ad2d3e7eff20
radufx <raduftw01@gmail.com>
2023-11-19 13:03:31 +0200

    refactor: final changes

--- myedoctor-web/src/components/Investigation/Suggestions.tsx
+++ myedoctor-web/src/components/Investigation/Suggestions.tsx
@@ -2,13 +2,12 @@ const Suggestions = () => {
   return (
     <div className=" mb-[-150px] flex max-w-[600px] flex-col gap-2 rounded-lg border-[0.5px] border-gray-300 p-5 text-start text-sm">
       <span className="text-base font-medium">
-        Don't know what to ask about? Here are some sample questions to help you get an idea.
+        Don&#39;t know what to ask about? Here are some sample questions to help you get an idea.
       </span>
       <ul>
         <li>What could be causing persistent headaches and fatigue?</li>
         <li>Is there a common explanation for sudden weight loss and increased thirst?</li>
-        <li>What might be the underlying issue if I'm experiencing chest pain and shortness of breath?</li>
-        <li>What might be the underlying issue if I'm experiencing chest pain and shortness of breath?</li>
+        <li>What might be the underlying issue if I&#39;m experiencing chest pain and shortness of breath?</li>
       </ul>
     </div>
   );
